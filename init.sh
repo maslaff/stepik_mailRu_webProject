7b94676f39ec1c8fde569650659e0225214e3cdf
MaSlaff <maslaff@yandex.ru>
2020-05-15 14:51:43 +0500

    init modified

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
-#sudo apt-get -y install nginx
-sudo pip install gunicorn
-mkdir /home/box/web
+# sudo apt-get -y install nginx
+pip3 install gunicorn
 cd /home/box/web
 mkdir -p public/img public/css public/js uploads
+sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+gunicorn -w 4 -b '0.0.0.0:8080' hello:app
+# sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+# sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/mysql start
