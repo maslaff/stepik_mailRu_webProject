ac588d5ba4c10ac516bd5af2458200eee9fb203b
MaSlaff <maslaff@yandex.ru>
2020-05-15 14:47:26 +0500

    cheanged for step 1.9

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
-sudo apt-get -y install nginx
+#sudo apt-get -y install nginx
+sudo pip install gunicorn
+mkdir /home/box/web
 cd /home/box/web
 mkdir -p public/img public/css public/js uploads
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
